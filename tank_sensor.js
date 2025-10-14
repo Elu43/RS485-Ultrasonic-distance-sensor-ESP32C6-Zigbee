3d3d0bff7e03c7557e0891d994907b6a1d99b694
Aralox <Aralox@users.noreply.github.com>
2025-10-14 12:28:28 +1100

    Update tank_sensor.js to use sensor instead of numeric
    fixes 'unavailable' problem

--- tank_sensor.js
+++ tank_sensor.js
@@ -1,4 +1,4 @@
-const {deviceEndpoints, numeric} = require('zigbee-herdsman-converters/lib/modernExtend');
+const {deviceEndpoints, sensor} = require('zigbee-herdsman-converters/lib/modernExtend');
 
 const definition = {
     zigbeeModel: ['esp32c6'],
@@ -6,7 +6,7 @@ const definition = {
     vendor: 'ESPRESSIF',
     description: 'Ultrasonic distance sensor. Use reporting minimum interval to set sample period in seconds. Reportable change is measured in 0.01 cm.',
     extend: [
-        numeric({
+        sensor({
             name: 'Distance',
             cluster: 'msIlluminanceMeasurement',
             attribute: 'measuredValue',
