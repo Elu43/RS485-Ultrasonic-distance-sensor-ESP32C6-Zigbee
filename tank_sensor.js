e516edf6ca10c60b9760837ed0803a990582ed90
Aralox <Aralox@users.noreply.github.com>
2025-10-23 16:52:20 +1100

    Change sensor to numeric for tank sensor definition

--- tank_sensor.js
+++ tank_sensor.js
@@ -1,4 +1,4 @@
-const {deviceEndpoints, sensor} = require('zigbee-herdsman-converters/lib/modernExtend');
+const {deviceEndpoints, numeric} = require('zigbee-herdsman-converters/lib/modernExtend');
 
 const definition = {
     zigbeeModel: ['esp32c6'],
@@ -6,7 +6,7 @@ const definition = {
     vendor: 'ESPRESSIF',
     description: 'Ultrasonic distance sensor. Use reporting minimum interval to set sample period in seconds. Reportable change is measured in 0.01 cm.',
     extend: [
-        sensor({
+        numeric({
             name: 'Distance',
             cluster: 'msIlluminanceMeasurement',
             attribute: 'measuredValue',
@@ -16,7 +16,7 @@ const definition = {
             precision: 2,
             scale: 100,
             reporting: {min: 5, max: 0, change: 100},
-            //access: 0b101
+            access: 'STATE_GET'
         })
     ],
     meta: {},
